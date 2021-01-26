9061cb160273fa6c2820cb4d0d534a59081bcb8e
Selwyn Horne-Primus <Selwyn_AnthonyHP@live.ca>
2021-01-26 08:40:06 -0500

    Added UnitTesting

---
+++ UnitTestProjectInventory/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Hi_Tech_Management_System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectInventory
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddUserTest()
+        {
+            User user = new User();
+        }
+    }
+}
